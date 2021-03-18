e1c782ea47615edb57dd6e188afac148d2510dda
chiangwill <b41510@gmail.com>
2021-03-18 16:16:25 +0800

    試著修正切換語系功能在 heroku 上無法正確執行的問題

--- pwb/settings.py
+++ pwb/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'pwb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.static',
             ],
         },
     },
@@ -127,9 +131,9 @@ TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
-USE_L10N = True
+# USE_L10N = True
 
-USE_TZ = True
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
